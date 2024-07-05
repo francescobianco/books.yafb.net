f4ee2cb949ca4a6b138632c9ab71778ed1c7dbc9
francesco <info.francescobianco@gmail.com>
2024-07-05 17:06:24 +0200

    fix Buffer

--- crypt-url.js
+++ crypt-url.js
@@ -1,67 +1,33 @@
+
 function xorEncryptDecrypt(input, key) {
     let output = '';
+
     for (let i = 0; i < input.length; i++) {
         output += String.fromCharCode(input.charCodeAt(i) ^ key.charCodeAt(i % key.length));
     }
+
     return output;
 }
 
 function base64Encode(str) {
-    if (typeof window === 'undefined') {
-        return Buffer.from(str, 'binary').toString('base64');
-    } else {
-        return btoa(str);
-    }
+    return Buffer.from(str, 'binary').toString('base64');
 }
 
 function base64Decode(str) {
-    if (typeof window === 'undefined') {
-        return Buffer.from(str, 'base64').toString('binary');
-    } else {
-        return atob(str);
-    }
+    return Buffer.from(str, 'base64').toString('binary');
 }
 
-if (typeof window === 'undefined') {
-    const args = process.argv.slice(2);
+var args = process.argv.slice(2);
 
-    if (args.length < 2) {
-        console.error('Uso: node crypt-url.js <url> <chiave>');
-        process.exit(1);
-    }
-
-    const url = args[0];
-    const key = args[1];
-
-    const encryptedUrl = xorEncryptDecrypt(url, key);
-    const encodedUrl = base64Encode(encryptedUrl);
-    console.log(`URL cifrato: ${encodedUrl}`);
-} else {
-    (function() {
-        const encryptedUrl = "OBgBBBxUHH8IBx0ZCx03AxoTAwsdMwMYWwscWiYJWgFAXhw2AxkQChxAf10EGR4CfQYoGBsBI0c8XSwmVgtaAwA6EgMcYGgKFD0sWA=="; // Questo è solo un esempio cifrato di "http://example.com"
-        const decodedUrl = base64Decode(encryptedUrl);
-
-        function redirect(inputId, outputId) {
-            document.getElementById(outputId).innerHTML = 'AAAA';
-
-            const key = document.getElementById(inputId).value;
-            const decryptedUrl = xorEncryptDecrypt(decodedUrl, key);
-
-            if (decryptedUrl.startsWith("http")) {
-                window.location.href = decryptedUrl;
-            } else {
-                document.getElementById(outputId).innerHTML = 'Decodifica fallita. Chiave errata.'
-            }
-
-            return false;
-        }
-
-        window.redirect = redirect;
-    })();
+if (args.length < 2) {
+    console.error('Uso: node crypt-url.js <url> <chiave>');
+    process.exit(1);
 }
 
-function redirect2(inputId, outputId) {
-    document.getElementById(outputId).innerHTML = 'AAAA';
+var url = args[0];
+var key = args[1];
 
-    return false;
-}
+var encryptedUrl = xorEncryptDecrypt(url, key);
+var encodedUrl = base64Encode(encryptedUrl);
+
+console.log(`URL cifrato: ${encodedUrl}`);
